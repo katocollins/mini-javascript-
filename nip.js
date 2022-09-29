a6e9127bd433bbf49c30d8e77957225c105be725
katocollins <108077573+katocollins@users.noreply.github.com>
2022-09-29 22:23:39 +0300

    New Project Here

---
+++ nip.js
@@ -0,0 +1,12 @@
+var image_input = document.getElementById('image_input');
+
+
+image_input.addEventListener("change", function()
+{
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      const uploaded_image = reader.result;
+       document.getElementById('display_image').style.backgroundImage = `url(${uploaded_image})`;
+    });
+     reader.readAsDataURL(this.files[0]);
+});
---
+++ upload.js
@@ -0,0 +1,12 @@
+const image_input = document.querySelector("#image_input");
+
+
+image_input.addEventListener("change", function()
+{
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      const uploaded_image = reader.result;
+       document.queryselector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
+    });
+     reader.readAsDataURL(this.files[0]);
+});
